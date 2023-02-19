9db0e5a173456feb81eb23da2e9062cf416ee695
Ismail Bayoussef <bayoussefismail@gmail.com>
2023-02-19 01:56:53 +0100

    add copying configuration

--- src/components/ComponentGrid/ComponentCard/ComponentCard.tsx
+++ src/components/ComponentGrid/ComponentCard/ComponentCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Flex } from "../../Flex/Flex";
 import Typography from "../../Typography/Typography";
 import StatusPill from "../../StatusPill/StatusPill";
@@ -6,14 +6,22 @@ import copy from "../../../assets/copy.svg";
 import { Component } from "../../../Enums/Component";
 import { sliceID } from "../../../hooks/useSliceID";
 import { useFormatDate } from "../../../hooks/useFormatDate";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-toastify";
 interface ComponentCardProps {
   component: Component;
 }
 
 const ComponentCard: React.FC<ComponentCardProps> = ({ component }) => {
+  const [copied, setCopied] = useState<boolean>(false);
   const { type, name, flavor, created, user, configuration, is_shared } =
     component;
-
+  useEffect(() => {
+    if (copied) {
+      toast.success(() => "Configuration Copied");
+      setCopied(false);
+    }
+  }, [copied]);
   return (
     <Flex direction="column" className="card" gap={10}>
       <Flex direction="row" justify="space-between" align="center">
@@ -45,8 +53,21 @@ const ComponentCard: React.FC<ComponentCardProps> = ({ component }) => {
         </Flex>
       </Flex>
       <Flex direction="row" justify="flex-start" gap={11} align="center">
-        <Typography fontSize={1.2}>Copy Configuration</Typography>
-        <img src={copy} alt="copy" />
+        <Typography fontSize={1.2}>
+          {JSON.stringify(configuration) !== "{}"
+            ? "Copy Configuration"
+            : "Configuration not available"}
+        </Typography>
+        {JSON.stringify(configuration) !== "{}" && (
+          <CopyToClipboard
+            text={JSON.stringify(configuration)}
+            onCopy={() => {
+              setCopied(true);
+            }}
+          >
+            <img src={copy} alt="copy" />
+          </CopyToClipboard>
+        )}
       </Flex>
     </Flex>
   );
